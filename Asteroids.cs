c43ee25bed39eb2a58000139f414ef1b06c1846f
SimranPPatil <sppatil2@illinois.edu>
2017-01-07 00:23:53 +0400

    Script for animating the Asteroids

---
+++ Asteroids.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Asteroids : MonoBehaviour {
+    private float maxRotationSpeed;
+    private Vector3 rotSpeed;
+    // Use this for initialization
+    void Start () {
+        maxRotationSpeed = 25;
+        rotSpeed = Random.insideUnitSphere * maxRotationSpeed;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        GetComponent<Transform>().Rotate(rotSpeed * Time.deltaTime);
+    
+    }
+}
