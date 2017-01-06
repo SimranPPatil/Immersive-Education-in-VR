2cee238b512421eb889f38e5da793e90bb69cf5d
SimranPPatil <sppatil2@illinois.edu>
2017-01-07 00:21:36 +0400

    Standard scripts
    These are the default scripts from Standard Assets or Asset store
    samples

---
+++ MovementXbox.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MovementXbox : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButton("Home"))
+            SceneManager.LoadScene("withfps");
+
+
+        Vector3 pos = transform.localPosition;
+        Vector3 rot = transform.eulerAngles;
+
+        pos.z += Input.GetAxis("Oculus_GearVR_LThumbstickX") * Time.deltaTime;
+        pos.x -= Input.GetAxis("Oculus_GearVR_LThumbstickY") * Time.deltaTime;
+
+        rot.y -= Input.GetAxis("Oculus_GearVR_RThumbstickY") * 80 * Time.deltaTime;
+
+        transform.localPosition = pos;
+        transform.eulerAngles = rot;
+    }
+}
