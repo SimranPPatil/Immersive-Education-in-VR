174fdafcef4c2b80d59dc63886d203e5567a8a34
SimranPPatil <sppatil2@illinois.edu>
2017-01-07 00:28:55 +0400

    Correct answer visualization in the quatgame

---
+++ Star.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Star : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	transform.Rotate(Vector3.down * 200, 75 * Time.deltaTime);
+    }
+}
